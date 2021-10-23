ea909c7205241d6d2b1874f86d8e7bf4d73cec27
horsicq <horsicq@gmail.com>
2021-10-23 03:42:59 +0200

    Fix: 2021-10-23

--- dialogsignatures.cpp
+++ dialogsignatures.cpp
@@ -105,9 +105,9 @@ int DialogSignatures::_handleTreeItems(QTreeWidgetItem *pParent,XBinary::FT file
 
     QList<DiE_ScriptEngine::SIGNATURE_RECORD> *pListSignatures=pDieScript->getSignatures();
 
-    int nNumberOfSignatures=pListSignatures->count();
+    qint32 nNumberOfSignatures=pListSignatures->count();
 
-    for(int i=0;i<nNumberOfSignatures;i++)
+    for(qint32 i=0;i<nNumberOfSignatures;i++)
     {
         if(pListSignatures->at(i).fileType==fileType)
         {
