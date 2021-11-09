a4021386f4bb9177ec96102437cbb1c0aa2e31be
horsicq <horsicq@gmail.com>
2021-11-09 20:07:42 +0100

    Fix: 2021-11-09

--- die_widget.cmake
+++ die_widget.cmake
@@ -6,16 +6,16 @@ include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialogtextinfo.cmake)
 set(DIE_WIDGET_SOURCES
     ${DIE_SCRIPT_SOURCES}
     ${DIALOGTEXTINFO_SOURCES}
-    ${CMAKE_CURRENT_LIST_DIR}/dialogdiescandirectory.ui
-    ${CMAKE_CURRENT_LIST_DIR}/dialogdiescanprocess.ui
-    ${CMAKE_CURRENT_LIST_DIR}/dialoglog.ui
-    ${CMAKE_CURRENT_LIST_DIR}/dialogsignatures.ui
-    ${CMAKE_CURRENT_LIST_DIR}/die_widget.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogdiescandirectory.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogdiescandirectory.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogdiescanprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogdiescanprocess.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialoglog.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialoglog.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogsignatures.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogsignatures.ui
     ${CMAKE_CURRENT_LIST_DIR}/die_highlighter.cpp
     ${CMAKE_CURRENT_LIST_DIR}/die_signatureedit.cpp
     ${CMAKE_CURRENT_LIST_DIR}/die_widget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/die_widget.ui
 )
