c38e5130bac0bade009839c04612f9118f857a8e
horsicq <horsicq@gmail.com>
2024-11-28 18:29:35 +0100

    Update file(s): 2024-11-28

--- die_widget.cmake
+++ die_widget.cmake
@@ -1,13 +1,17 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../die_script/die_script.cmake)
+if (NOT DEFINED DIE_SCRIPT_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../die_script/die_script.cmake)
+    set(DIE_WIDGET_SOURCES ${DIE_WIDGET_SOURCES} ${DIE_SCRIPT_SOURCES})
+endif()
+
 include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialogtextinfo.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/xdialogprocess.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../FormatDialogs/dialogfindtext.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../Controls/xcomboboxex.cmake)
 
 set(DIE_WIDGET_SOURCES
-    ${DIE_SCRIPT_SOURCES}
+    ${DIE_WIDGET_SOURCES}
     ${DIALOGTEXTINFO_SOURCES}
     ${XDIALOGPROCESS_SOURCES}
     ${DIALOGFINDTEXT_SOURCES}
